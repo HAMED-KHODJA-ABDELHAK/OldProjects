54f4fd0b47612c0baf8651d65d312f22f9af43e2
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2013-01-30 15:00:10 -0500

    Adding latest programs.

--- fun/master.c
+++ fun/master.c
@@ -5,6 +5,7 @@
 /* C Headers */
 #include <stdio.h>
 #include <stdlib.h> 
+#include <unistd.h>
 
 /* Project Headers */ 
 #include "mpi.h"
@@ -35,16 +36,42 @@
 
 
 /****************************** Global Functions **************************************************/
+void error(char *str) {
+	printf("Error: %s.\n", str);
+	exit(1);
+}
+
 /**
  * Main loop of the function.
  */
 int main(int argc, char **argv) {
-	int rank, size;
-	char command[] = "worker";
+	int rank, size, uni_size, *uni_sizep, flag;
+	char worker_name[] = "worker";
+	MPI_Comm everyone;
+
 	MPI_Init(&argc, &argv);
 
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	MPI_Comm_get_attr(MPI_COMM_WORLD, MPI_UNIVERSE_SIZE, &uni_sizep, &flag);
+
+	if (!flag) {
+		printf("This MPI does not support uni_size. How many processes total?\n");
+		scanf("%d", &uni_size);
+	} else {
+		uni_size = *uni_sizep;
+	}
+
+	if (uni_size == 1) 
+		error("No room to start workers.");
+	
+	MPI_Comm_spawn(worker_name, MPI_ARGV_NULL, uni_size-1,
+			MPI_INFO_NULL, 0, MPI_COMM_SELF, &everyone
+			MPI_ERRCODES_IGNORE);
+
+	sleep(10);
+	printf("Master woke up.\n");
 
 	MPI_Finalize();
 	
--- fun/worker.c
+++ fun/worker.c
@@ -39,16 +39,32 @@
 
 
 /****************************** Global Functions **************************************************/
+void error(char *str) {
+	printf("Error: %s.\n", str);
+	exit(1);
+}
+
 /**
  * Main loop of the function.
  */
 int main(int argc, char **argv) {
 	int rank, size;
+	MPI_Comm parent;
 
 	MPI_Init(&argc, &argv);
+	MPI_Comm_get_parent(&parent);
 
+	if (parent == MPI_COMM_NULL)
+		error("No parent.");
+
+	MPI_Comm_remote_size(parent, &size);
+	if (size != 1)
+		error("Something wrong with parent.");
+	
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	printf("I am a worker number %d of %d.\n", rank, size);
 
 	MPI_Finalize();
 	
