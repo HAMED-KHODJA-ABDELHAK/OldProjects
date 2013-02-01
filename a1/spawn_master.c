c35ab21059e5509f6f3398663fb3f09acd6d1743
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2013-02-01 18:56:31 -0500

    Adding timer to spawn for master.

--- a1/spawn_master.c
+++ a1/spawn_master.c
@@ -38,11 +38,11 @@ int main(int argc, char **argv) {
 	MPI_Comm everyone;
 	int rank, size, tasks, darts, hits = 0, all_hits = 0;
 	char str_rounds[RNDS_LEN];
-	double start, pi;
+	double start_init, start_spawn, pi;
 
 	/* Standard startup. */
 	MPI_Init(&argc, &argv);
-	start = MPI_Wtime();
+	start_init = MPI_Wtime();
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
@@ -54,19 +54,21 @@ int main(int argc, char **argv) {
 	snprintf(str_rounds, RNDS_LEN, "%d", (int) darts/tasks);
 	str_rounds[RNDS_LEN-1] = '\0';
 	char *w_args[] = {str_rounds, NULL};
-	printf("Spawning %d slaves to throw %s.\n", str_rounds);
+	printf("Spawning %d slaves to throw %s.\n", tasks, str_rounds);
 
 	/* Spawn workers then block with reduce. */
+	start_spawn = MPI_Wtime();
 	MPI_Comm_spawn(SLAVE, w_args, tasks,
 			MPI_INFO_NULL, 0, MPI_COMM_SELF, &everyone,
 			MPI_ERRCODES_IGNORE);
+	printf("Time just for comm spawn is %.10f seconds.\n", MPI_Wtime() - start_spawn);
 
 	/* Call reduce, this master contributes 0. recv_buf has total once finished. */
 	MPI_Reduce(&hits, &all_hits, 1, MPI_INT, MPI_SUM, MPI_ROOT, everyone);
 	pi = (4.0 * all_hits / darts);
 	printf("The hit count was %d, the final value of PI is: %.40f.\n", all_hits, pi);
 	printf("The percent deviation from reference: %.10f%%\n", (pi - REAL_PI)/REAL_PI *100);
-	printf("Time elapsed from MPI_Init to MPI_Finalize is %.10f seconds.\n", MPI_Wtime() - start);
+	printf("Time elapsed from MPI_Init to MPI_Finalize is %.10f seconds.\n", MPI_Wtime() - start_init);
 
 	MPI_Finalize();
 
