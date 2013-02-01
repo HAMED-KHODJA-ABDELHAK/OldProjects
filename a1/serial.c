7f7de83f8b5f03b7f0d59b3117ac128ad5adb4ef
Jeremy Pallats <cirrus@cluster.com>
2013-02-01 16:06:37 -0500

    Cleaning up serial code.

--- a1/serial.c
+++ a1/serial.c
@@ -1,5 +1,8 @@
 /**
- * Template C file. 
+ * Serial impelmentation of pi calculation, used as baseline.
+ * Use command: bsub -I -q COMP428 -n1 mpirun -srun ./demo/serial darts 
+ * Arguments to serial:
+ * darts: Number of darts to throw, basically the work load.
  */
 /****************************** Header Files ******************************************************/
 /* C Headers */
@@ -11,7 +14,7 @@
 #include "mpi.h"
 
 /****************************** Constants/Macros **************************************************/
-#define RADIUS 0.5
+#define RADIUS 1.0 
 
 /****************************** Type Definitions **************************************************/
 
@@ -26,13 +29,27 @@
 
 
 /****************************** Global Functions **************************************************/
+void usage() {
+	printf("Usage: ./demo/serial darts\ndarts: The number of darts to throw.\n");
+	exit(1);
+}
+
+/*
+ * Determines if a point is inside a circle of radius centered on origin. 
+ * X and Y are assumed to come in as numbers between 0 and 1 from rand.
+ * If the distance from center is less than OR equal to radius, we say it is inside.
+ */
 int in_circle(double x, double y, double r) {
-    double cent_x = x-r, cent_y = y-r;
+    double cent_x = 2*x-r, cent_y = 2*y-r;
     double dist = (cent_x * cent_x) + (cent_y * cent_y);
 
     return dist <= r*r;
 }
 
+/*
+ * Function goes through a number of rnds, each time randomly gets a pair of x,y coords that
+ * are inside a 1x1 square. Checks if point is in circle, if so increments cnt. 
+ */
 int throw_darts(unsigned long rnds) {
 	double x, y;
 	int cnt = 0;
@@ -49,27 +66,31 @@ int throw_darts(unsigned long rnds) {
 }
 
 /**
- * Main loop of the function.
+ * Main programming body, executes the dart throwing and reports to user the time taken. 
  */
 int main(int argc, char **argv) {
 	int rank, size, cnt, rnds;
 	double start;
 
-	MPI_Init(&argc, &argv);	
+	/* Start timing before init. */
 	start = MPI_Wtime();
+
+	/* Standard init for MPI. */
+	MPI_Init(&argc, &argv);	
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
-
 	srand(time(NULL));
 
+	/* Get workload from command argument and execute. */
+	if (argc < 2)
+		usage();
 	rnds = atoi(*++argv);
-	printf("Num darts is: %d.\n", rnds);
 	cnt = throw_darts(rnds);
 
+	/* Report final pi calculation, cleanup and report time taken. */
     printf("The number count was %d, the final value of PI is: %.40f.\n", cnt, (4.0 * cnt / rnds));
-	printf("This program took %f seconds to complete.\n", MPI_Wtime() - start);
-
 	MPI_Finalize();
+	printf("This program took %f seconds to complete.\n", MPI_Wtime() - start);
 
 	return 0;
 }
