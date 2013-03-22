7cdd67b9b5aa296031bf5964b88bd5c94ef00088
Jeremy Pallats <cirrus@cluster.com>
2013-03-22 14:07:08 -0400

    Adding a3 test files.

---
+++ a3/lib_floyd.hpp
@@ -0,0 +1,62 @@
+#ifndef _LIB_FLOYD_HPP_
+#define _LIB_FLOYD_HPP_
+
+/********************* Header Files ***********************/
+/* C++ Headers */
+//#include <iostream> /* Input/output objects. */
+//#include <fstream> /* File operations. */
+//#include <sstream> /* String stream. */
+//#include <string> /* C++ String class. */
+//#include <new> /* Defines bad_malloc exception, new functions. */
+//#include <typeinfo> /* Casting header. */
+//#include <exception> /* Top level exception header. */
+//#include <stdexcept> /* Derived exception classes. */
+
+/* STL Headers */
+//#include <vector>
+//#include <list>
+//#include <deque>
+//#include <stack>
+//#include <queue>
+//#include <priority_queue>
+//#include <bitset>
+//#include <set> // multiset for multiple keys allowed.
+//#include <map> // multimap for multiple keys allowed.
+//#include <utility> // Has pair for map.
+//#include <algorithm>
+//#include <numeric>
+
+/* C Headers */
+//#include <cstdlib>
+//#include <cstddef>
+//#include <cctype>
+//#include <cstring>
+//#include <cstdio>
+//#include <climits>
+//#include <cassert>
+
+/* Project Headers */
+
+/******************* Constants/Macros *********************/
+
+
+/******************* Type Definitions *********************/
+/* For enums: Try to namesapce the common elements. 
+ * typedef enum {
+ *	VAL_,
+ * } name_e; 
+ */
+
+/* For structs: 
+ * typedef struct name_s {
+ *	int index;
+ * } name_t;  
+ */
+
+/****************** Class Definitions *********************/
+
+
+/********************* Prototypes *************************/
+
+
+#endif /* _LIB_FLOYD_HPP_ */
