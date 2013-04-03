ebd873532d398c4b76ae99fc9df0e0ba55d3ae74
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2013-04-03 09:41:24 -0400

    New group method.

--- newa3/random.c
+++ newa3/random.c
@@ -56,41 +56,56 @@ int main(int argc, char **argv) {
     if(world != 4)
         lib_error("MAIN: Fail.");
 
-    int num_rows = lib_sqrt(world), list[num_rows];
-    /* Row comm at the front, col comm at the back. */
-    MPI_Comm comm[num_rows*2];
-    MPI_Group MPI_WORLD_GROUP, group;
-    MPI_Comm_group(MPI_COMM_WORLD, &MPI_WORLD_GROUP);
+    int num_rows = lib_sqrt(world);
+    int mesg_r = rank * 10;
+    int mesg_c = rank * 20;
 
-    /* Row comms. */
-    for (int i = 0; i < num_rows; ++i) {
-        lib_get_group_list(num_rows, i, 1, list);
-        MPI_Group_incl(MPI_WORLD_GROUP, num_rows, list, &group);
-        MPI_Comm_create(MPI_COMM_WORLD, group, &comm[i]);
-        MPI_Group_free(&group);
-    }
+    MPI_Comm comm_r, comm_c;
 
-    /* Column comms. */
-    for (int i = 0; i < num_rows; ++i) {
-        lib_get_group_list(num_rows, i, 0, list);
-        MPI_Group_incl(MPI_WORLD_GROUP, num_rows, list, &group);
-        MPI_Comm_create(MPI_COMM_WORLD, group, &comm[num_rows+i]);
-        MPI_Group_free(&group);
-    }
+    MPI_Comm_split(MPI_COMM_WORLD, rank/num_rows, rank, &comm_r);
+    MPI_Comm_split(MPI_COMM_WORLD, rank%num_rows, rank, &comm_c);
 
-    int mesg = 0;
-    if (rank == 2)
-        mesg = 10;
+    MPI_Bcast(&mesg_r, 1, MPI_INT, 0, comm_r);
+    MPI_Bcast(&mesg_c, 1, MPI_INT, 1, comm_c);
 
-    MPI_Bcast(&mesg, 1, MPI_INT, 0, comm[1]);
+    printf("I am %d and I have from row %d and column %d.\n", rank, mesg_r, mesg_c);
 
-    printf("I am %d, I have  %d.\n", rank, mesg);
+//    /* Row comm at the front, col comm at the back. */
+//    MPI_Comm comm_r, comm_c;
+//    MPI_Group MPI_WORLD_GROUP, group;
+//    MPI_Comm_group(MPI_COMM_WORLD, &MPI_WORLD_GROUP);
+//
+//    /* Row comms. */
+//    for (int i = 0; i < num_rows; ++i) {
+//        lib_get_group_list(num_rows, i, 1, list);
+//        MPI_Group_incl(MPI_WORLD_GROUP, num_rows, list, &group);
+//        MPI_Comm_create(MPI_COMM_WORLD, group, &comm[i]);
+//        MPI_Group_free(&group);
+//    }
+//
+//    /* Column comms. */
+//    for (int i = 0; i < num_rows; ++i) {
+//        lib_get_group_list(num_rows, i, 0, list);
+//        MPI_Group_incl(MPI_WORLD_GROUP, num_rows, list, &group);
+//        MPI_Comm_create(MPI_COMM_WORLD, group, &comm[num_rows+i]);
+//        MPI_Group_free(&group);
+//    }
+//
+//    int mesg = 0;
+//    if (rank == 2)
+//        mesg = 10;
+//
+//    MPI_Bcast(&mesg, 1, MPI_INT, 0, comm[1]);
+//
+//    printf("I am %d, I have  %d.\n", rank, mesg);
+//
+//    MPI_Group_free(&MPI_WORLD_GROUP);
+//    for (int i = 0; i < num_rows*2; ++i) {
+//        MPI_Comm_free(comm+i);
+//    }
 
-    MPI_Group_free(&MPI_WORLD_GROUP);
-    for (int i = 0; i < num_rows*2; ++i) {
-        MPI_Comm_free(comm+i);
-    }
-
+    MPI_Comm_free(&comm_r);
+    MPI_Comm_free(&comm_c);
     MPI_Finalize();
 
     return 0;
